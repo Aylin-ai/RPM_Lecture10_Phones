ec2488d8f43f7c5eaa54d74ca36224cde552b0f7
Aylin-ai <69709234+Aylin-ai@users.noreply.github.com>
2022-04-27 20:01:04 +0300

    Add files via upload

---
+++ IDialogService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phones
+{
+    public interface IDialogService
+    {
+        public void ShowMessage(string message);   // показ сообщения
+        public string FilePath { get; set; }   // путь к выбранному файлу
+        public bool OpenFileDialog();  // открытие файла
+        public bool SaveFileDialog();  // сохранение файла
+    }
+}
+    
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace Phones
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new ApplicationViewModel
+                (
+                new DefaultDialogService(), 
+                new JsonFileService()
+                );
+        }
+    }
+}
